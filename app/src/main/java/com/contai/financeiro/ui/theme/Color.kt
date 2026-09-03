c8b4b7bae4ed0663f80e311d419f99eaaaa6828f
Pedroxs11 <pvs_store@hotmail.com>
2026-09-03 14:20:17 -0300

    Adiciona paleta escura da V1.1

--- app/src/main/java/com/contai/financeiro/ui/theme/Color.kt
+++ app/src/main/java/com/contai/financeiro/ui/theme/Color.kt
@@ -14,5 +14,11 @@ val ContaiTextSecondary = Color(0xFF667085)
 
 val ContaiIncome = Color(0xFF1677FF)
 val ContaiExpense = Color(0xFFD94A4A)
-
 val ContaiWarning = Color(0xFFF2A93B)
+
+val ContaiDarkBackground = Color(0xFF101312)
+val ContaiDarkSurface = Color(0xFF171C1A)
+val ContaiDarkSurfaceVariant = Color(0xFF202724)
+val ContaiDarkTextPrimary = Color(0xFFF2F4F3)
+val ContaiDarkTextSecondary = Color(0xFFAAB3AF)
+val ContaiDarkGreenContainer = Color(0xFF153B2E)
