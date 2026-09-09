e3b3e48d137a61c14d8171fc488b03579932b35d
Pedroxs11 <pvs_store@hotmail.com>
2026-09-08 22:14:01 -0300

    Prepara versionCode 5 para teste interno

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.contai.financeiro"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
+        versionCode = 5
         versionName = "1.1-beta"
     }
 
