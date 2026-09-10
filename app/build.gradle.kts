4c8127377c459d698b869ab76ee5ad5e63de8cae
Pedroxs11 <pvs_store@hotmail.com>
2026-09-10 17:14:41 -0300

    Prepara build interno versionCode 7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.contai.financeiro"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
+        versionCode = 7
         versionName = "1.1-beta"
     }
 
